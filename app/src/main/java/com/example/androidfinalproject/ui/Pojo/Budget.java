a3e067f352e796ceddeb39cf8f8e96860fe23051
Stefan Marceta <w0660242@myscc.ca>
2024-02-28 16:29:42 -0500

    added a pojo/javabean

---
+++ app/src/main/java/com/example/androidfinalproject/ui/Pojo/Budget.java
@@ -0,0 +1,18 @@
+package com.example.androidfinalproject.ui.Pojo;
+
+public class Budget {
+
+    private int id;
+
+    public Budget(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
