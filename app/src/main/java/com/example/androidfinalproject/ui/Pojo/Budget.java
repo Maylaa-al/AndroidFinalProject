83031691d6b09d34fbb02ba89350c8d3b607f076
JavaNerd1995 <w0660242@myscc.ca>
2024-04-01 13:56:33 -0400

    fixing pojo to updated version

--- app/src/main/java/com/example/androidfinalproject/ui/Pojo/Budget.java
+++ app/src/main/java/com/example/androidfinalproject/ui/Pojo/Budget.java
@@ -1,15 +1,25 @@
 package com.example.androidfinalproject.ui.Pojo;
 
-public class Budget {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
+/**
+ *  POJO/JavaBean to represent some form of data that the app will be built around*/
+public class Budget implements Parcelable {
 
     private int id;
     private Double price;
     private String fromCurrency;
-
     private String toCurrency;
-
     private String date;
+    private String accountNum;
+    private String category;
+    private double amount;
+    private String notes;
 
+    // Create object to read from the database
     public Budget(int id, Double price, String fromCurrency, String toCurrency, String date) {
         this.id = id;
         this.price = price;
@@ -24,6 +34,29 @@ public class Budget {
         this.toCurrency = toCurrency;
         this.date = date;
     }
+
+    public Budget(int id, String date, String accountNum, String category, double amount, String notes) {
+        this.id = id;
+        this.date = date;
+        this.accountNum = accountNum;
+        this.category = category;
+        this.amount = amount;
+        this.notes = notes;
+    }
+
+    public Budget() {
+
+    }
+
+//    public Budget(String date, String accountNum, String category, double amount, String notes) {
+//        this.date = date;
+//        this.accountNum = accountNum;
+//        this.category = category;
+//        this.amount = amount;
+//        this.notes = notes;
+//    }
+
+
 
     public int getId() {
         return id;
@@ -65,4 +98,85 @@ public class Budget {
     public void setDate(String date) {
         this.date = date;
     }
+
+    public String getAccountNum() {
+        return accountNum;
+    }
+
+    public void setAccountNum(String accountNum) {
+        this.accountNum = accountNum;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel dest, int flags) {
+        dest.writeInt(this.id);
+        dest.writeString(this.date);
+        dest.writeString(this.accountNum);
+        dest.writeString(this.category);
+        dest.writeDouble(this.amount);
+        dest.writeString(this.notes);
+
+    }
+
+    public void readFromParcel(Parcel source) {
+        this.id = source.readInt();
+        this.date = source.readString();
+        this.accountNum = source.readString();
+        this.category = source.readString();
+        this.amount = source.readDouble();
+        this.notes = source.readString();
+    }
+
+    protected Budget(Parcel in) {
+        this.id = in.readInt();
+        this.date = in.readString();
+        this.accountNum = in.readString();
+        this.category = in.readString();
+        this.amount = in.readDouble();
+        this.notes = in.readString();
+    }
+
+    public static final Parcelable.Creator<Budget> CREATOR = new Parcelable.Creator<Budget>() {
+        @Override
+        public Budget createFromParcel(Parcel source) {
+            return new Budget(source);
+        }
+
+        @Override
+        public Budget[] newArray(int size) {
+            return new Budget[size];
+        }
+    };
+
+
+
 }
